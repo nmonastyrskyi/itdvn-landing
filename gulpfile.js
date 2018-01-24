e0e5941d78ffec82805621cb058de69143c7a836
Nikita Monastyrskyy <n.monastyrskyi@gmail.com>
2018-01-24 14:22:36 +0200

    fix approach css positioning

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,87 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var pug = require('gulp-pug');
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var spritesmith = require('gulp.spritesmith');
-var rimraf = require('rimraf');
-var rename = require("gulp-rename");
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const spritesmith = require('gulp.spritesmith');
+const rimraf = require('rimraf');
+const rename = require('gulp-rename');
 
 
-				// SERVER 
+
+
+/* -------- Server  -------- */
 gulp.task('server', function() {
-    browserSync.init({
-        server: {
-        	port: 9000,
-            baseDir: "app"
-        }
-    });
+  browserSync.init({
+    server: {
+      port: 9000,
+      baseDir: "build"
+    }
+  });
 
-    gulp.watch('app/**/*').on('change', browserSync.reload)
+  gulp.watch('build/**/*').on('change', browserSync.reload);
 });
 
-			// PUG COMPILE
+/* ------------ Pug compile ------------- */
 gulp.task('templates:compile', function buildHTML() {
-  return gulp.src('src/template/index.pug')
-  .pipe(pug({
-    pretty: true
-  }))
-  .pipe(gulp.dest('app'))
+  return gulp.src('source/template/index.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('build'))
 });
 
-		//CSS BUILD
-gulp.task('css:build', function () {
-    return gulp.src('src/styles/main.scss')
-    
-      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-      .pipe(autoprefixer({
-      browsers: ['last 2 versions'], 
-      cascade: false
-      }))
-      .pipe(rename('main.min.css'))
-    
-    .pipe(gulp.dest('app/css'));
+/* ------------ Styles compile ------------- */
+gulp.task('styles:compile', function () {
+  return gulp.src('source/styles/main.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('build/css'));
 });
 
-
-
-		// SPRITE
-gulp.task('sprite', function (cb) {
-  var spriteData = gulp.src('src/img/icons/*.png').pipe(spritesmith({
+/* ------------ Sprite ------------- */
+gulp.task('sprite', function(cb) {
+  const spriteData = gulp.src('source/images/icons/*.png').pipe(spritesmith({
     imgName: 'sprite.png',
-    imgPath: '../img/sprite.png',
+    imgPath: '../images/sprite.png',
     cssName: 'sprite.scss'
   }));
-  spriteData.img.pipe(gulp.dest('app/img/'));
-  spriteData.css.pipe(gulp.dest('src/styles/global/'));
+
+  spriteData.img.pipe(gulp.dest('build/images/'));
+  spriteData.css.pipe(gulp.dest('source/styles/global/'));
   cb();
 });
- 
 
-//DELETE
+
+
+/* ------------ Delete ------------- */
 gulp.task('clean', function del(cb) {
-  return rimraf('app', cb);
+  return rimraf('build', cb);
 });
 
-//COPY FONTS
-gulp.task('copy:fonts', function(){
-	return gulp.src('./src/fonts/**/*.*')
-	.pipe(gulp.dest('app/fonts'))
-})
+/* ------------ Copy fonts ------------- */
+gulp.task('copy:fonts', function() {
+  return gulp.src('./source/fonts/**/*.*')
+    .pipe(gulp.dest('build/fonts'));
+});
 
-//COPY IMAGES
-gulp.task('copy:images', function(){
-return gulp.src('./src/img/**/*.*')
-.pipe(gulp.dest('app/img'))
-})
+/* ------------ Copy images ------------- */
+gulp.task('copy:images', function() {
+  return gulp.src('./source/img/**/*.*')
+    .pipe(gulp.dest('build/img'));
+});
 
-//COPY
-gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'))
+/* ------------ Copy ------------- */
+gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
 
-//WATCHERS
-gulp.task('watch', function(){
-	gulp.watch('src/template/**/*.pug', gulp.series('templates:compile'));
-		gulp.watch('src/styles/**/*.scss', gulp.series('css:build'))
-})
+/* ------------ Watchers ------------- */
+gulp.task('watch', function() {
+  gulp.watch('source/template/**/*.pug', gulp.series('templates:compile'));
+  gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
+});
 
 gulp.task('default', gulp.series(
-	'clean',
-	gulp.parallel('templates:compile', 'css:build', 'sprite', 'copy'),
-	gulp.parallel('watch', 'server')
-	)
+  'clean',
+  gulp.parallel('templates:compile', 'styles:compile', 'sprite', 'copy'),
+  gulp.parallel('watch', 'server')
+  )
 );
